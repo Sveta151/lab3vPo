16f51701734a088e935750278b04c02e72a2ca18
Sveta151 <46090129+Sveta151@users.noreply.github.com>
2018-12-23 20:59:33 +0400

    Add files via upload

---
+++ ClassSet.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+namespace lab3OneMoreTime
+{
+    public class ClassSet
+    {
+
+
+        public List<int> Summ(List<int> r1, List<int> r2)
+        {
+            int size1 = r1.Count;
+            int size2 = r2.Count;
+            List<int> r3 = new List<int>();
+
+
+
+            if (size1 >= size2)
+            {
+                for (int i = 0; i < size1; i++)
+                {
+                    r3.Add(r1[i]);
+                }
+                for (int i = 0; i < size2; i++)
+                {
+                    int k = 0;
+                    bool isIn = false;
+                    while (k < r3.Count)
+                    {
+                        
+                        if (r3[k] == r2[i])
+                        {
+                            isIn = true;
+                        }
+                        k++;
+                    }
+                    if (isIn == false)
+                    {
+                        r3.Add(r2[i]);
+                    }
+
+                }
+            }
+            else
+            {
+                for (int i = 0; i < size2; i++)
+                {
+                    r3.Add(r2[i]);
+                }
+                for (int i = 0; i < size1; i++)
+                {
+                    int k = 0;
+                    bool isIn = false;
+                    while (k < r3.Count)
+                    {
+                        if (r3[k] == r1[i])
+                        {
+                            isIn = true;
+                        }
+                        k++;
+                    }
+                    if (isIn == false)
+                    {
+                        r3.Add(r1[i]);
+                    }
+
+                }
+
+               
+            }
+            r3.Sort();
+            return r3;
+        }
+
+        public List<int> Peresech (List<int>r1, List<int>r2)
+        {
+            int size1 = r1.Count;
+            int size2 = r2.Count;
+            List<int> r3 = new List<int>();
+
+                for(int i=0; i<size1; i++)
+                {
+                    bool isIn = false;
+                    for (int j=0; j<size2; j++)
+                    {
+
+                        if (r2[j]==r1[i])
+                        {
+                            isIn = true;
+                        }
+                    }
+                    if(isIn == true)
+                    {
+                        r3.Add(r1[i]);
+                    }
+                }
+
+            r3.Sort();
+            return r3;
+
+        }
+        public List<int> Substraction(List<int>r1, List<int>r2)
+        {
+            List<int> r3 = new List<int>();
+            int size1 = r1.Count;
+            int size2 = r2.Count;
+            for(int i=0; i<size1; i++)
+            {
+                bool isIn = false;
+                for ( int j=0; j<size2; j++)
+                {
+                    if (r2[j] == r1[i])
+                    {
+                        isIn = true;
+                    }
+                }
+                if (isIn == false)
+                {
+                    r3.Add(r1[i]);
+                }
+
+            }
+            return r3;
+        }
+        public bool Isin(List<int>r1, int r2)
+        {
+            bool answer = false;
+            int size1 = r1.Count;
+            for (int i=0; i<size1; i++)
+            {
+                if(r1[i]==r2)
+                {
+                    answer = true;
+                  }
+
+            }
+            return answer; 
+        }
+
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+namespace lab3OneMoreTime
+{
+    [TestFixture]
+    public class Test
+    {
+        ClassSet S = new ClassSet();
+        [Test]
+        public void firsttest1()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> b2 = new List<int> { 7, 8, 9 };
+            List<int> b3 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Assert.AreEqual(S.Summ(b1, b2), b3);
+
+        }
+        [Test]
+        public void firsttest2()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4 };
+            List<int> b2 = new List<int> { 4, 5, 6, 7, 8, 9 };
+            List<int> b3 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Assert.AreEqual(S.Summ(b1, b2), b3);
+
+        }
+
+        [Test]
+        public void secondtest()
+        {
+            List<int> b1 = new List<int> { 4, 5 };
+            List<int> b2 = new List<int> { 4, 5 };
+            List<int> b3 = new List<int> { 4, 5 };
+
+            Assert.AreEqual(S.Peresech(b1, b2), b3);
+
+        }
+        [Test]
+        public void secondtest2()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> b2 = new List<int> { 4, 5, 6, 7 };
+            List<int> b3 = new List<int> { 4, 5 };
+
+            Assert.AreEqual(S.Peresech(b1, b2), b3);
+
+        }
+        [Test]
+        public void secondtest3()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> b2 = new List<int> { 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            List<int> b3 = new List<int> { 4, 5 };
+
+            Assert.AreEqual(S.Peresech(b1, b2), b3);
+
+        }
+
+        [Test]
+        public void thirdtest()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> b2 = new List<int> { 4, 5, 6, 7 };
+            List<int> b3 = new List<int> { 1, 2, 3 };
+
+            Assert.AreEqual(S.Substraction(b1, b2), b3);
+
+        }
+        [Test]
+        public void thirdtest2()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> b2 = new List<int> { 6, 7 };
+            List<int> b3 = new List<int> { 1, 2, 3, 4, 5 };
+
+            Assert.AreEqual(S.Substraction(b1, b2), b3);
+
+        }
+        [Test]
+        public void fourthtest()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4, 5 };
+            int b2 = 3;
+            bool b3 = true;
+
+            Assert.AreEqual(S.Isin(b1, b2), b3);
+
+        }
+        [Test]
+        public void fourthtest2()
+        {
+            List<int> b1 = new List<int> { 1, 2, 3, 4, 5 };
+            int b2 = 6;
+            bool b3 = false;
+
+            Assert.AreEqual(S.Isin(b1, b2), b3);
+
+        }
+    }
+}
